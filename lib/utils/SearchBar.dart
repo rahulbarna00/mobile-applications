a33cfaa1d6793645aba92f8d1f93f187b9e1f01e
Rahul Barna <rahulbarna@gmail.com>
2023-11-16 17:46:25 +0530

    Search Bar Design

--- lib/utils/SearchBar.dart
+++ lib/utils/SearchBar.dart
@@ -6,18 +6,19 @@ class CustomSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextEditingController searchController = TextEditingController();
+    Brightness systemBrightness = MediaQuery.of(context).platformBrightness;
 
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20.0), // Set border radius for curvature
+          color: systemBrightness == Brightness.light ? Colors.white : Colors.white70,
+          borderRadius: BorderRadius.circular(28.0), // Set border radius for curvature
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.4), // Set shadow color
               spreadRadius: 2,
-              blurRadius: 5,
+              blurRadius: 6,
               offset: const Offset(0, 2), // Adjust the offset to control shadow position
             ),
           ],
@@ -26,6 +27,9 @@ class CustomSearchBar extends StatelessWidget {
           controller: searchController,
           decoration: const InputDecoration(
             hintText: 'Search',
+            hintStyle: TextStyle(
+              color: Colors.black87
+            ),
             prefixIcon: Icon(Icons.search, color: Colors.black),
             border: InputBorder.none, // Remove the default border
           ),
